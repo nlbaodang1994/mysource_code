bf1c9512806c4985faf3156e6b9653c9722e22e8
nlbaodang1994 <nlbaodang1994@gmail.com>
2015-10-04 23:52:51 +0700

    sửa combobox

--- ReportSource/Report_Test/Report_Test/ExcelFormatOption.cs
+++ ReportSource/Report_Test/Report_Test/ExcelFormatOption.cs
@@ -18,39 +18,7 @@ namespace Report_Test
 
             InitializeComponent();
         }
-        private void LoadCombo()
-        {
-            ExcelFormatOptions exp = new ExcelFormatOptions();
-        }
-
-        private void cboColWidOb_SelectedValueChanged(object sender, EventArgs e)
-        {
-            int stt = 0;
-            string ask = null;
-            AreaOptions _aOpts=new AreaOptions(stt,ask);
-            try
-            {
-                AreaOptions[] list = new AreaOptions[]
-            {
-                new AreaOptions(1, AreaSectionKind.WholeReport.ToString()),
-                new AreaOptions(2, AreaSectionKind.ReportHeader.ToString()),
-                new AreaOptions(3, AreaSectionKind.PageHeader.ToString()),
-                new AreaOptions(4, AreaSectionKind.GroupHeader.ToString()),
-                new AreaOptions(6, AreaSectionKind.Detail.ToString()),
-                new AreaOptions(7, AreaSectionKind.GroupFooter.ToString()),
-                new AreaOptions(8, AreaSectionKind.PageFooter.ToString()),
-                new AreaOptions(9, AreaSectionKind.ReportFooter.ToString()),
-            };
-                cboColWidOb.DataSource = list;
-                cboColWidOb.DisplayMember = ask;
-                cboColWidOb.ValueMember = ask;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        
-        }
+     
 
         private void ExcelFormatOption_Load(object sender, EventArgs e)
         {
